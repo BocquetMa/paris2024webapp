f9840b86646d2a7303f8320369297fb30c66d063
BocquetMa <matheo.bocquet18@gmail.com>
2024-09-23 16:53:52 +0200

    Création de fichier pour afficher les Olympiades correspondant à un athlete

---
+++ src/main/java/bts/sio/webapp/model/Olympiade.java
@@ -0,0 +1,16 @@
+package bts.sio.webapp.model;
+
+
+import lombok.Data;
+
+import java.time.LocalDate;
+
+@Data
+public class Olympiade {
+
+    private Integer id;
+    private String numero;
+    private Integer annee;
+    private String ville;
+
+}
